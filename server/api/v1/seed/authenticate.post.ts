6d95ba7dac8d950543b6db7a5191fbd20374b043
venetc <markedone1992@gmail.com>
2023-12-22 18:29:08 +0700

    CF fix

--- server/api/v1/seed/authenticate.post.ts
+++ server/api/v1/seed/authenticate.post.ts
@@ -1,8 +1,11 @@
 export default defineEventHandler(async (event) => {
   const { secret } = await readBody<{ secret: string }>(event)
-  const { seedToken } = useRuntimeConfig()
 
-  if (secret !== seedToken) return createError({ statusCode: 401, statusMessage: 'Invalid token' })
+  const cloudflareContext = event.context.cloudflare
+
+  const token = cloudflareContext ? cloudflareContext.NUXT_SEED_TOKEN : useRuntimeConfig().seedToken
+
+  if (secret !== token) return createError({ statusCode: 401, statusMessage: 'Invalid token' })
 
   return { status: 'ok' }
 })
